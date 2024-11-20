82085fe104da56c4bc58c5603117e466c40991a0
Dilier Esteban Fortaleche Lopez <dilierestebanfortalechelopez@gmail.com>
2024-11-19 22:06:30 -0500

    cree la funcion agregar

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ listaDeExperimentos = [
 
 def agregar_experimento():
     """Permite agregar un nuevo experimento con sus atributos. dificultad: 1"""
+    #trabajando
     pass
 
 def eliminar_experimentos():
